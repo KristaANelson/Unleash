cf35aa684fcd3d2494dffa3627cab87a2df83198
KristaANelson <krista.a.nelson@gmail.com>
2015-03-09 10:40:16 -0600

    hound fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "welcome/index"
   root to: "welcome#index"
   resources :users
-  get 'auth/:provider/callback', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get "auth/:provider/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
